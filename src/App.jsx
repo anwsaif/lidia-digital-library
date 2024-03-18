374f474c93180450949d44c952c488afe5348794
Anwar S <anwarxsaifullah+git@gmail.com>
2024-03-18 13:37:59 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl font-extrabold text-primary'>Hijau Betawi / Jingga Bis Kota
+Kuning Gigi Balang / Biru Abang
+Pink None & City Collaboration.</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"PlusJakartaSans"', 'monospace']
+      },
+      colors: {
+        link: {
+          active: "#4737FF",
+          inactive: "#9A9EA6"
+        },
+        heading: "#181E4B",
+        desc: "#5E6282",
+        primary: "#4475F2",
+        placeholder: "#9C9C9C",
+        border: "#DEDEDE"
+      }
+    },
+  },
+  plugins: [],
+}
+
