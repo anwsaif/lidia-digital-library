05055842a8f95d01b496bce857e1bd6aca76f790
Anwar S <anwarxsaifullah+git@gmail.com>
2024-04-08 10:04:28 +0700

    Update Header: make it responsive

--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -34,16 +34,13 @@ const Header = () => {
   return (
     <header>
       <div className="container mx-auto p-2">
-        <div className="flex items-center justify-between">
-          <div className="z-50" >
+        <div className="flex items-center justify-between md:p-3">
+          <div className="z-50 md:hidden" >
           <Hamburger onToggle={onHamburgerClickHandler} toggled={isOpen} toggle={setOpen} size={24} />
           </div>
           <img src={logo} alt="Lidia"/>
-          <button >
-            <FeatherIcon icon='log-in' className='mr-2' />
-          </button>
-          <nav className='absolute left-0 top-0 hidden h-screen w-screen items-center justify-center bg-white opacity-0 transition duration-300 ease-in-out'>
-            <ul className='flex flex-col justify-between gap-6'>
+          <nav className='absolute left-0 top-0 hidden h-screen w-screen items-center justify-center bg-white opacity-0 transition duration-300 ease-in-out md:static md:left-auto md:top-auto md:flex md:h-auto md:w-auto md:opacity-100'>
+            <ul className='flex flex-col justify-between gap-6 md:flex-row'>
               <li>
                 <a href="#features" onClick={onNavLinkClickHandler}>Features</a></li>
               <li>
@@ -54,6 +51,10 @@ const Header = () => {
                 <a href="#location">Location</a></li>
             </ul>
           </nav>
+          <button className='rounded-[0.625rem] md:bg-white'>
+            <FeatherIcon icon='log-in' className='mr-2 md:hidden' />
+            <span className='mx-12 my-2 hidden font-bold leading-6 text-heading md:block'>Login</span>
+          </button>
         </div>
       </div>
     </header>
