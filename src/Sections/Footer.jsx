f62946036d9d2576406df473131447055ac2e0a6
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-10 09:35:57 +0700

    Update Footer: fix spacing

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -8,20 +8,26 @@ function Footer() {
   return (
     <section className='2xl:mt-96' id='services'>
       <div className="container mx-auto my-12 px-4 py-2 md:px-4 lg:my-[6.25rem] lg:px-5 lg:py-7 xl:px-10">
-        <div className="mx-auto flex w-fit flex-wrap gap-24">
+        <div className="mx-auto flex w-fit flex-wrap gap-12 lg:gap-24">
           <div className="flex flex-col gap-4 lg:items-center lg:gap-6">
             <span className='subtitle-1 lg:text-2xl'>Managed By</span>
-            <img src={logo} alt="Brand Logo"/>
+            <img src={logo} alt="Brand Logo" className='w-20 lg:w-auto'/>
           </div>
-          <div className="flex flex-col gap-4 lg:items-center lg:gap-6">
+          <div className="flex flex-col gap-4 lg:items-center lg:gap-14">
             <span className='subtitle-1 lg:text-2xl'>Social Media</span>
             <div className="flex gap-[1.125rem]">
-              <img src={xtwitter} alt="X/Twitter"/>
-              <img src={instagram} alt="Instagram"/>
-              <img src={facebook} alt="Facebook"/>
+              <a href="#">
+                <img src={xtwitter} alt="X/Twitter"/>
+              </a>
+              <a href="#">
+                <img src={instagram} alt="Instagram"/>
+              </a>
+              <a href="#">
+                <img src={facebook} alt="Facebook"/>
+              </a>
             </div>
           </div>
-          <div className="flex flex-col gap-4 lg:items-center lg:gap-6">
+          <div className="flex flex-col gap-4 lg:items-center lg:gap-14">
             <span className='subtitle-1 lg:text-2xl'>Slogan</span>
             <h6>#RentFavBooks</h6>
           </div>
