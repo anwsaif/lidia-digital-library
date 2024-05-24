a4dc011234514059f7293829f6c3da9424403ae9
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-24 09:52:11 +0700

    Update Navbar: fix z-index(s)

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,8 @@ function App() {
 
   return (
     <>
-      <div className="relative min-h-[97vh]">
-        <div className='absolute right-0 top-0 h-[26rem] w-[75vw] bg-[url("./assets/Cover.svg")] bg-cover bg-no-repeat min-[516px]:w-[64vw] sm:w-[55vw] md:h-[107vh] lg:max-w-[37.875rem] xl:h-[130vh] xl:max-w-none '></div>
-        <Navbar />
-        <Hero />
-      </div>
+      <Navbar />
+      <Hero />
       <Features />
       <Services />
       <Reviews />
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
   // }
   return (
     <section className='md:h-[85vh]'>
-      <div className="container mx-auto flex h-full px-4 py-2 md:px-4 lg:px-5 lg:py-7 xl:px-10 ">
+      <div className="container mx-auto flex h-full px-4 py-16 md:px-4 md:pt-52 lg:px-5 lg:py-7 xl:px-10 ">
         <div className="relative flex w-full flex-col-reverse items-center md:flex-row md:gap-x-7 ">
           <div className="flex flex-col gap-4 md:w-1/2 md:gap-8">
             <h1 className='text-heading'>Search & review your <span className='text-primary underline'>fav book</span> effortlessly</h1>
@@ -16,6 +16,7 @@ function Hero() {
           </div>
           <img src={Left} alt="Books" className='min-[529px]:max-w-96 w-screen max-w-[23rem] md:w-full lg:w-1/2 lg:max-w-full'/>
         </div>
+        <div className='absolute right-0 top-0 -z-30 h-[26rem] w-[75vw] bg-[url("./assets/Cover.svg")] bg-cover bg-no-repeat min-[516px]:w-[64vw] sm:w-[55vw] md:h-[107vh] lg:max-w-[37.875rem] xl:h-[130vh] xl:max-w-none '></div>
       </div>
     </section>
   )
