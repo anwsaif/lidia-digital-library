b9b22fac4b692f6b8796223b083d798ca3808d6c
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-25 11:40:02 +0700

    Update Reviews: add aos

--- src/Sections/Reviews.jsx
+++ src/Sections/Reviews.jsx
@@ -32,8 +32,8 @@ function Reviews() {
     <section className='2xl:mt-96' id='reviews'>
       <div className="container mx-auto my-12 px-4 py-2 md:px-4 lg:my-[6.25rem] lg:px-5 lg:py-7 xl:px-10">
         <div className="flex w-full flex-col gap-4 ">
-          <span className='capitalised text-xs text-primary'>REVIEWS</span>
-          <h2>💬• Reviews of Others</h2>
+          <span data-aos="fade-up" className='capitalised text-xs text-primary'>REVIEWS</span>
+          <h2 data-aos="fade-up" data-aos-duration="1300">💬• Reviews of Others</h2>
 
           <div className="flex flex-wrap justify-center gap-9 lg:mt-12 lg:gap-[3.1875rem] xl:mt-[5.625rem] 2xl:justify-around ">
             {reviews.map((data) => {
