1abce6f92bb1165c88ceb1b78f8cad98f7a75e46
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-25 11:40:25 +0700

    Update Footer: add aos

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -6,7 +6,7 @@ import facebook from '../assets/images/socials/Facebook(1).svg'
 
 function Footer() {
   return (
-    <footer className='2xl:mt-96' id='footer'>
+    <footer data-aos="fade-zoom-in" data-aos-duration="2000" className='2xl:mt-96' id='footer'>
       <div className="container mx-auto my-12 px-4 py-2 md:px-4 lg:my-[6.25rem] lg:px-5 lg:py-7 xl:px-10">
         <div className="mx-auto flex w-fit flex-wrap gap-12 lg:gap-24">
           <div className="flex flex-col gap-4 lg:items-center lg:gap-6">
