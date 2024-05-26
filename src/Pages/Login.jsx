9d994c9d7f8ce57b43e54b79a2619cf40c348aa1
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-26 20:59:05 +0700

    Update Login: add form tag

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -9,11 +9,13 @@ function Login() {
         <p className='mb-6 text-desc'><span className='text-primary'>Home</span> / Login</p>
         <h6 className='mb-4 text-heading'>Login</h6>
         <p className='mb-10 text-desc'>Input your username and password.</p>
-        <label htmlFor="username">Username</label>
-        <input type="text" name='username'  id='username'/>
-        <label htmlFor="password" className='mt-7'>Password</label>
-        <input type="text" name='password'  id='password'/>
-        <button className='mt-7 w-full rounded-[4px] bg-primary py-4 text-sm font-bold text-white'>Login</button>
+        <form action="#">
+          <label htmlFor="username">Username</label>
+          <input type="text" name='username'  id='username'/>
+          <label htmlFor="password" className='mt-6'>Password</label>
+          <input type="text" name='password'  id='password'/>
+          <button className='mt-7 w-full rounded-[4px] bg-primary py-4 text-sm font-bold text-white'>Login</button>
+        </form>
       </div>
       <div className='mt-10 flex flex-col gap-10 text-sm text-desc'>
         <p>Don't have an account? <a href="#" className='text-link-active hover:text-desc'>Create One!</a></p>
