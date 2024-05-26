be626f801643a110dd9bcf49cfd76ce8b6ff5bb7
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-26 20:57:35 +0700

    Update Login: separate py-(s) for xl and 2xl, center image and form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,9 +3,9 @@ import logo from '../assets/images/logo/Logo-round-big.png'
 
 function Login() {
   return (
-    <div id='login' className="container grid h-screen place-items-center px-2 py-4 lg:py-10">
-      <img src={logo} alt=""/>
-      <div className="w-[400px] rounded-[5px] bg-white px-9 py-10 drop-shadow-normal-dark">
+    <div id='login' className="container mx-auto h-screen px-2 py-4 xl:py-7 2xl:py-10">
+      <img src={logo} alt="" className='mx-auto'/>
+      <div className="mx-auto w-[400px] rounded-[5px] bg-white px-9 pb-5 pt-10 drop-shadow-normal-dark xl:mt-6 2xl:mt-12">
         <p className='mb-6 text-desc'><span className='text-primary'>Home</span> / Login</p>
         <h6 className='mb-4 text-heading'>Login</h6>
         <p className='mb-10 text-desc'>Input your username and password.</p>
