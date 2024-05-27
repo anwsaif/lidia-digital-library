0b918fc24e44715b01610bcc3b28d19143118a7e
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-27 10:49:05 +0700

    Update Login: fix spacing for specific breakpoints

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,23 +3,25 @@ import logo from '../assets/images/logo/Logo-round-big.png'
 
 function Login() {
   return (
-    <div id='login' className="container mx-auto h-screen px-2 py-4 xl:py-7 2xl:py-10">
-      <img src={logo} alt="" className='mx-auto'/>
-      <div className="mx-auto w-[400px] rounded-[5px] bg-white px-9 pb-5 pt-10 drop-shadow-normal-dark xl:mt-6 2xl:mt-12">
-        <p className='mb-6 text-desc'><span className='text-primary'>Home</span> / Login</p>
-        <h6 className='mb-4 text-heading'>Login</h6>
-        <p className='mb-10 text-desc'>Input your username and password.</p>
-        <form action="#">
-          <label htmlFor="username">Username</label>
-          <input type="text" name='username'  id='username'/>
-          <label htmlFor="password" className='mt-6'>Password</label>
-          <input type="text" name='password'  id='password'/>
-          <button className='mt-7 w-full rounded-[4px] bg-primary py-4 text-sm font-bold text-white'>Login</button>
-        </form>
-      </div>
-      <div className='mt-10 flex flex-col gap-10 text-center text-sm text-desc'>
-        <p>Don't have an account? <a href="#" className='text-link-active hover:text-desc'>Create One!</a></p>
-        <span>© 2024 Lidia. All rights reserved.</span>
+    <div id='login' className="container mx-auto h-screen px-2 py-5 xl:py-7 2xl:py-10">
+      <div className="flex h-fit flex-col items-center justify-center min-[1920px]:h-full">
+        <img src={logo} alt="" className='max-w-[100px] '/>
+        <div className="w-max-[400px] mt-5 w-fit rounded-[5px] border-t-[1.5px] border-primary bg-white px-6 pb-4 pt-4 drop-shadow-normal-dark md:px-8 lg:px-9 lg:pt-10 xl:mt-6 2xl:mt-12">
+          <p className='mb-6 text-desc'><span className='text-primary'>Home</span> / Login</p>
+          <h6 className='mb-4 text-heading'>Login</h6>
+          <p className='mb-10 text-desc'>Input your username and password.</p>
+          <form action="#">
+            <label htmlFor="username">Username</label>
+            <input autoFocus type="text" name='username'  id='username'/>
+            <label htmlFor="password" className='mt-6'>Password</label>
+            <input type="text" name='password'  id='password'/>
+            <button className='mt-7 w-full rounded-[4px] bg-primary py-4 text-sm font-bold text-white'>Login</button>
+          </form>
+        </div>
+        <div className='my-10 flex flex-col gap-10 text-center text-sm text-desc'>
+          <p>Don't have an account? <a href="#" className='text-link-active hover:text-desc'>Create One!</a></p>
+          <span>© 2024 Lidia. All rights reserved.</span>
+        </div>
       </div>
     </div>  
   )
