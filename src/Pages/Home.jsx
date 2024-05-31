3109047b9cbd9b3e0fee89b1a3d76fa865c61722
Anwar S <anwarsaifullah+code@yandex.com>
2024-05-31 10:32:45 +0000

    add preloader to Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import Navbar from '../Sections/Navbar'
-import Hero from '../Sections/Hero'
-import Features from '../Sections/Features'
-import Services from '../Sections/Services'
-import Reviews from '../Sections/Reviews'
-import Location from '../Sections/Location'
-import Footer from '../Sections/Footer'
+import React, { useEffect, useState } from "react";
+import Navbar from "../Sections/Navbar";
+import Hero from "../Sections/Hero";
+import Features from "../Sections/Features";
+import Services from "../Sections/Services";
+import Reviews from "../Sections/Reviews";
+import Location from "../Sections/Location";
+import Footer from "../Sections/Footer";
+import { Cube } from "react-preloaders";
 
 function Home() {
+  const [screenLoading, setScreenLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenLoading(false);
+    }, 3000);
+  }, []);
   return (
     <>
-      <Navbar/>
-      <div className="w-full h-[84px] xl:h-[124px] max-lg:hidden "></div>
-      <Hero/>
-      <Features />
-      <Services />
-      <Reviews />
-      <Location />
-      <Footer />
+      {screenLoading ? (
+        <Cube color="rgb(68, 117, 242)" />
+      ) : (
+        <>
+          <Navbar />
+          <div className="h-[84px] w-full max-lg:hidden xl:h-[124px]"></div>
+          <Hero />
+          <Features />
+          <Services />
+          <Reviews />
+          <Location />
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
