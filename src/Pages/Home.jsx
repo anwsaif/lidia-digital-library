9af6f396dfff84364db85ea472c46eda33888083
Anwar S <anwarsaifullah+code@yandex.com>
2024-06-01 10:09:03 +0700

    fix preloader

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import Navbar from "../Sections/Navbar";
 import Hero from "../Sections/Hero";
 import Features from "../Sections/Features";
@@ -10,18 +10,21 @@ import { Cube } from "react-preloaders";
 
 function Home() {
   const [screenLoading, setScreenLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setScreenLoading(false);
-    }, 3000);
+  useLayoutEffect(() => {
     document.getElementsByTagName('body')[0].style.position = 'static'
     document.getElementsByTagName('body')[0].style.overflowY = 'auto'
-  }, []);
+    setTimeout(() => {
+      document.getElementById('preloader').style.opacity = 0
+      setTimeout(() => {
+        setScreenLoading(false);
+      }, 1000)
+    }, 2000);
+  });
   return (
     <>
       {screenLoading ? (
         <Cube color="rgb(68, 117, 242)" />
-      ) : (
+      ) :'' }
         <>
           <Navbar />
           <div className="h-[84px] w-full max-lg:hidden xl:h-[124px]"></div>
@@ -32,7 +35,6 @@ function Home() {
           <Location />
           <Footer />
         </>
-      )}
     </>
   );
 }
